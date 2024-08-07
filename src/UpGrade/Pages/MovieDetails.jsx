931737bc5df0c7e81b0e9c830ea89bd7196b0a3f
Mhando-Z <Mhandosz17@gmail.com>
2024-08-08 01:42:29 +0300

    Phase 3

--- src/UpGrade/Pages/MovieDetails.jsx
+++ src/UpGrade/Pages/MovieDetails.jsx
@@ -1,8 +1,9 @@
 import axios from "axios";
+import Rating from "../Components/Rating";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import Rating from "../Components/Rating";
 import { IoPersonCircleOutline } from "react-icons/io5";
+import { FaRegCirclePlay } from "react-icons/fa6";
 
 function MovieDetails() {
   const [Details, setDetails] = useState([]);
@@ -87,21 +88,22 @@ function MovieDetails() {
     getSimilar();
   }, [Page, id]);
 
-  console.log(Details);
-  console.log(Images);
-
   return (
-    <div className="flex flex-col min-h-screen mt-20 font-roboto">
-      <div className="container flex flex-col mx-auto">
+    <div className="flex flex-col min-h-screen font-roboto">
+      <div className="flex flex-col">
         {/* backdrop poster */}
-        <img
-          src={`https://image.tmdb.org/t/p/w500/${Details?.backdrop_path}`}
-          alt={Details?.title}
-          className="object-cover w-screen h-[450px]"
-        />
-        <div className="flex flex-row gap-3">
+        <div className="relative">
+          <img
+            src={`https://image.tmdb.org/t/p/w500/${Details?.backdrop_path}`}
+            alt={Details?.title}
+            className="object-cover object-top w-screen xl:h-[550px] h-[500px]"
+          />
+          {/* Trallers section be here*/}
+          <div className="absolute top-0 bottom-0 left-0 right-0 flex flex-col bg-gradient-to-t from-black via-transparent to-transparent"></div>
+        </div>
+        <div className="flex flex-col gap-3 md:flex-row xl:container xl:mx-auto">
           {/* frontal image */}
-          <div className="bg-white h-[260px] rounded-lg mt-[-100px] ml-5">
+          <div className="md:h-[260px] h-[200px] hidden  z-40 justify-center md:flex rounded-lg mt-[-100px] ml-5">
             <img
               src={`https://image.tmdb.org/t/p/w500/${Details?.poster_path}`}
               alt={Details?.title}
@@ -109,11 +111,18 @@ function MovieDetails() {
             />
           </div>
           {/* title and prompt actions */}
-          <div>
+          <div className="flex flex-col flex-grow px-2">
             {/* movie title */}
-            <h1 className="mb-2 text-5xl font-bold text-gray-100">
+            <h1 className="max-w-lg mb-2 text-3xl font-bold text-gray-100 md:max-w-2xl md:text-5xl font-action">
               {Details?.title}
             </h1>
+            {/* Tagline */}
+            <h2 className="mb-2 text-base font-medium text-gray-100 xl:text-lg ">
+              TagLine:
+              <span className="max-w-2xl ml-1 font-romance text-cyan-400">
+                {Details?.tagline}
+              </span>
+            </h2>
             {/* movie genres */}
             <div className="flex flex-wrap items-center mb-2 md:flex-row gap-x-5">
               <h1 className="text-base font-medium text-gray-100 xl:text-lg">
@@ -166,10 +175,24 @@ function MovieDetails() {
               </h1>
             </div>
             {/* movie description */}
-            <p className="max-w-lg mt-2 text-gray-100">{Details?.overview}</p>
+            <p className="mt-2 mb-1 text-base font-medium text-cyan-400 xl:text-lg">
+              Overview
+            </p>
+            <p className="max-w-lg text-gray-100 xl:max-w-xl font-romance">
+              {Details?.overview}
+            </p>
           </div>
           {/* Action Buttons */}
-          <div></div>
+          <div className="flex flex-col items-end px-4 mt-2 gap-y-4">
+            <button className="flex flex-row items-center px-4 py-2 text-gray-100 bg-red-600 bg-opacity-65 gap-x-2">
+              <FaRegCirclePlay />
+              Watch
+            </button>
+            <button className="flex flex-row items-center px-4 py-2 text-gray-100 bg-green-600 bg-opacity-65 gap-x-1">
+              <FaRegCirclePlay />
+              Trailler
+            </button>
+          </div>
         </div>
         {/* Other movie images */}
         <div className="grid grid-cols-3 gap-1 mt-5">
@@ -185,24 +208,22 @@ function MovieDetails() {
             );
           })}
         </div>
+        {/* Other movies which are similar */}
 
         {/* Reviews */}
-        <div>
+        <div className="flex flex-col xl:container xl:mx-auto">
           {Reviews?.length === 0 ? (
             ""
           ) : (
             <div className="mt-5 h-[400px] overflow-auto bg-slate-900 bg-opacity-50">
               <div className="sticky top-0 flex border-b-2 bg-gray-950 border-slate-700">
-                <h1 className="px-10 mt-5 mb-5 text-2xl font-semibold text-yellow-500 md:text-3xl ">
+                <h1 className="mt-5 mb-5 text-2xl font-semibold text-yellow-500 md:text-3xl ">
                   Reviews
                 </h1>
               </div>
               {Reviews?.map((data, index) => {
                 return (
-                  <div
-                    key={index + data.author}
-                    className="flex flex-col px-10"
-                  >
+                  <div key={index + data.author} className="flex flex-col mt-5">
                     <h1 className="flex flex-row items-center w-full text-xl font-semibold gap-x-3 md:text-2xl text-slate-200">
                       <IoPersonCircleOutline className="text-3xl" />
                       {data.author}
