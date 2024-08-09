c586e20695b357dfb1cbf96b7315731cd9902233
Mhando-Z <Mhandosz17@gmail.com>
2024-08-09 16:10:59 +0300

    Phase 3

--- src/UpGrade/Pages/MainPage.jsx
+++ src/UpGrade/Pages/MainPage.jsx
@@ -3,8 +3,8 @@ import HeroSection from "./HeroSection";
 import TrendingMovies from "../MovieScreens/TrendingMovies";
 import TrendingSeries from "../TvSeriesScreens/TrendingSeries";
 import UpComingMovies from "../MovieScreens/UpComingMovies";
-import { Sentry } from "react-activity";
 import MovieListContext from "../../Context/MovieListContext";
+import { ImLoop2 } from "react-icons/im";
 
 function MainPage() {
   const { Trending } = useContext(MovieListContext);
@@ -12,7 +12,9 @@ function MainPage() {
   if (!Trending || Trending?.length === 0) {
     return (
       <div className="flex flex-col items-center justify-center min-h-screen">
-        <Sentry color="#727981" size={32} speed={1} animating={true} />
+        <div>
+          <ImLoop2 className="text-3xl text-gray-100 transition-all duration-700 opacity-70 md:text-4xl animate-spin" />
+        </div>
       </div>
     );
   }
--- src/UpGrade/Pages/TvSeriesPage.jsx
+++ src/UpGrade/Pages/TvSeriesPage.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import TvHeroSection from "../TvSeriesScreens/TvHeroSection";
 import TopRatedSeries from "../TvSeriesScreens/TopRatedSeries";
 import TodaySeies from "../TvSeriesScreens/TodaySeries";
 import TvSeriesList from "../TvSeriesScreens/TvSeriesList";
+import { ImLoop2 } from "react-icons/im";
+import TvSeriesContex from "../../Context/TvSeriesContext";
 
 function TvSeriesPage() {
+  const { Todaylist } = useContext(TvSeriesContex);
+
+  if (!Todaylist || Todaylist?.length === 0) {
+    return (
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <div>
+          <ImLoop2 className="text-3xl text-gray-100 transition-all duration-700 opacity-70 md:text-4xl animate-spin" />
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="flex flex-col gap-y-5">
       {/* TvHerosection */}
