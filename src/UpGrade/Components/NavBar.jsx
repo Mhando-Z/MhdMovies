3a5715d9435c264c267f0788073096a6037bb649
Mhando-Z <Mhandosz17@gmail.com>
2024-08-09 18:04:46 +0300

    Phase 3

--- src/UpGrade/Components/NavBar.jsx
+++ src/UpGrade/Components/NavBar.jsx
@@ -1,12 +1,14 @@
 import React, { useContext, useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import { MdLocationSearching } from "react-icons/md";
-import logo from "../../Assets/Logo/mhd.png";
 import { HiMenu } from "react-icons/hi";
 import MovieListContext from "../../Context/MovieListContext";
+import logo from "../../Assets/Logo/mhd.png";
+import { motion } from "framer-motion";
 
 function NavBar() {
   const [query, setQuery] = useState("");
+  const [view, setView] = useState(false);
   const navigate = useNavigate();
   const { visible } = useContext(MovieListContext);
 
@@ -30,11 +32,15 @@ function NavBar() {
       navigate(`/Results/${query}`);
     }
   };
+  // handleClick
+  const handleClick = () => {
+    setView(!view);
+  };
 
   return (
     <div className={`fixed top-0 left-0 right-0 z-50 flex flex-col ${visible}`}>
       <div className={`flex flex-col px-2 mt-3`}>
-        <div className="flex flex-row items-center justify-between gap-x-5 ">
+        <div className="relative flex flex-row items-center justify-between gap-x-5 ">
           {/* logo */}
           <div className="flex grow">
             <img
@@ -94,8 +100,73 @@ function NavBar() {
           </div>
           {/* humberger icon for small devices */}
           <div className="flex text-5xl text-red-600 md:hidden">
-            <HiMenu />
+            <HiMenu onClick={handleClick} />
           </div>
+          {/* navlinks for mobile phone */}
+          {view ? (
+            <div className="absolute bottom-0 left-0 right-0 md:hidden top-14 bg-opacity-30">
+              {/* navlinks */}
+              <motion.div
+                initial={{ opacity: 0, y: 20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                className="flex flex-row items-center justify-between px-2 py-1 bg-black bg-opacity-25 md:flex ring-1 ring-gray-400 rounded-3xl"
+              >
+                <NavLink
+                  to={"/"}
+                  className={({ isActive }) =>
+                    isActive
+                      ? "bg-red-500 py-1 text-gray-100 rounded-3xl px-5  "
+                      : "px-5 text-gray-100 font-medium hover:bg-red-500 hover:text-gray-100 rounded-3xl"
+                  }
+                >
+                  Home
+                </NavLink>
+                <NavLink
+                  to={"TvSeries/"}
+                  className={({ isActive }) =>
+                    isActive
+                      ? "bg-red-500  py-1 text-gray-100 rounded-3xl px-5  "
+                      : "px-5 text-gray-100 font-medium hover:bg-red-500 hover:text-gray-100 rounded-3xl"
+                  }
+                >
+                  TvSeries
+                </NavLink>
+                <NavLink
+                  to={"Genres"}
+                  className={({ isActive }) =>
+                    isActive
+                      ? "bg-red-500  py-1 text-gray-100 rounded-3xl px-5  "
+                      : "px-5 text-gray-100 font-medium hover:bg-red-500 hover:text-gray-100 rounded-3xl"
+                  }
+                >
+                  Genres
+                </NavLink>
+              </motion.div>
+              {/* search input */}
+              <motion.div
+                initial={{ opacity: 0, y: 20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ delay: 0.1 }}
+                className="relative w-full mt-3"
+              >
+                <input
+                  type="text"
+                  onKeyDown={(e) => handlePress(e)}
+                  onChange={(e) => handleChange(e)}
+                  className="px-10 py-2 text-gray-100 bg-black bg-opacity-25 outline-none ring-1 ring-gray-300 rounded-3xl focus:ring-2 focus:text-gray-100"
+                  placeholder="Search movies"
+                />
+                <div
+                  onClick={HandleSearch}
+                  className="absolute top-0 right-0 flex items-center justify-center bg-red-700 rounded-full size-10"
+                >
+                  <MdLocationSearching className="text-2xl text-center text-white" />
+                </div>
+              </motion.div>
+            </div>
+          ) : (
+            ""
+          )}
         </div>
       </div>
     </div>
