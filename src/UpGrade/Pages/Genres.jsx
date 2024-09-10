28e758f2b99237119d1db040101ddce120b7e1fa
Mhando-Z <Mhandosz17@gmail.com>
2024-09-10 16:41:43 +0300

    Phase 3

--- src/UpGrade/Pages/Genres.jsx
+++ src/UpGrade/Pages/Genres.jsx
@@ -6,6 +6,7 @@ import TvSeriesContex from "../../Context/TvSeriesContext";
 import MoviePoster from "../Components/MoviePoster";
 import TvSeriesPoster from "../Components/TvSeriesPoster";
 import { motion } from "framer-motion";
+import logo from "../../Assets/Logo/mhd.png";
 
 function Genres() {
   const { Collected } = useContext(MovieListContext);
@@ -25,6 +26,12 @@ function Genres() {
   const { genre } = TvGenre;
   const [count, setCount] = useState(14);
   const [counts, setCounts] = useState(14);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  // Function to toggle the sidebar
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
 
   //handle page incement
   const handleIncrese = () => {
@@ -74,73 +81,150 @@ function Genres() {
   });
 
   return (
-    <div className="flex flex-col justify-between min-h-screen px-2 mt-20 gap-y-5 ">
-      <div className="flex flex-col justify-between md:flex-row gap-y-4 gap-x-10">
-        {/* side panel */}
-        <div className="flex md:flex-col justify-between flex-row md:w-[200px] md:min-h-screen h-[200px] md:rounded bg-slate-600">
-          <div className="flex flex-col">
-            <div className="bg-slate-600 h-[60px]">
-              <div className="px-5 py-5">
-                <h1 className="text-lg font-bold md:text-xl text-slate-200">
-                  Movie Genres
-                </h1>
-              </div>
-            </div>
-            <div className="h-[340px] overflow-auto py-3 px-5">
-              {genres?.map((data, index) => {
-                return (
-                  <motion.div
-                    key={index + data.id}
-                    className={`${data.name === Gname ? Unselect : ""}`}
-                  >
-                    <h1
-                      onClick={() => HandleSelect(data.id, data.name)}
-                      className="px-4 text-lg font-semibold rounded cursor-pointer text-slate-200"
-                    >
-                      {data.name}
-                    </h1>
-                  </motion.div>
-                );
-              })}
-            </div>
-          </div>
-          <div className="flex flex-col">
-            <div className="bg-slate-600 h-[60px]">
-              <div className="px-5 py-5 ">
-                <h1 className="text-lg font-bold md:text-xl text-slate-200">
-                  Tv-Series Genres
-                </h1>
-              </div>
-            </div>
-            <div className="h-[300px] overflow-auto py-3 px-5">
-              {genre?.map((data, index) => {
-                return (
-                  <div
-                    key={index + data.id}
-                    className={`${data.name === Gnames ? Unselect1 : " "}`}
-                  >
-                    <h1
-                      onClick={() => HandleTvSelect(data.id, data.name)}
-                      className="px-4 text-lg font-semibold rounded cursor-pointer text-slate-200"
-                    >
-                      {data.name}
-                    </h1>
+    <div className="flex h-screen">
+      {/* Sidebar */}
+      <aside
+        className={`fixed top-0 left-0 w-64 bg-black h-full transform ${
+          isSidebarOpen ? "translate-x-0" : "-translate-x-full"
+        } md:translate-x-0 transition-transform duration-300 ease-in-out z-50`}
+      >
+        <div className="flex flex-col justify-between min-h-screen p-6">
+          <div>
+            <img src={logo} alt="herveg logo" className="h-10 " />
+            <nav className="mt-10">
+              <div className="flex flex-col w-full">
+                <div className="flex flex-col justify-between gap-y-4 ">
+                  {/* side panel */}
+                  <div className="flex flex-col justify-between md:flex-col">
+                    <div className="flex flex-col">
+                      <div className="">
+                        <div className="px-5 py-5">
+                          <h1 className="text-lg font-bold md:text-xl text-slate-200">
+                            Movie Genres
+                          </h1>
+                        </div>
+                      </div>
+                      <div className="h-[340px] overflow-auto py-3 px-5">
+                        {genres?.map((data, index) => {
+                          return (
+                            <motion.div
+                              key={index + data.id}
+                              className={`${
+                                data.name === Gname ? Unselect : ""
+                              }`}
+                            >
+                              <h1
+                                onClick={() => HandleSelect(data.id, data.name)}
+                                className="px-4 text-lg font-semibold rounded cursor-pointer text-slate-200"
+                              >
+                                {data.name}
+                              </h1>
+                            </motion.div>
+                          );
+                        })}
+                      </div>
+                    </div>
+                    <div className="flex flex-col">
+                      <div className="">
+                        <div className="px-5 py-5 ">
+                          <h1 className="text-lg font-bold md:text-xl text-slate-200">
+                            Tv-Series Genres
+                          </h1>
+                        </div>
+                      </div>
+                      <div className="h-[300px] overflow-auto py-3 px-5">
+                        {genre?.map((data, index) => {
+                          return (
+                            <div
+                              key={index + data.id}
+                              className={`${
+                                data.name === Gnames ? Unselect1 : " "
+                              }`}
+                            >
+                              <h1
+                                onClick={() =>
+                                  HandleTvSelect(data.id, data.name)
+                                }
+                                className="px-4 text-lg font-semibold rounded cursor-pointer text-slate-200"
+                              >
+                                {data.name}
+                              </h1>
+                            </div>
+                          );
+                        })}
+                      </div>
+                    </div>
                   </div>
-                );
-              })}
-            </div>
+                </div>
+              </div>
+            </nav>
           </div>
         </div>
-        <div className="flex-1 h-[900px] flex flex-col   justify-center overflow-auto rounded-xl ring-4 ring-slate-700">
+      </aside>
+
+      {/* Overlay for Sidebar on small screens */}
+      {isSidebarOpen && (
+        <div
+          className="fixed inset-0 z-40 bg-black opacity-50"
+          onClick={toggleSidebar}
+        ></div>
+      )}
+
+      {/* Main content */}
+      <div className="flex-1 ml-0 overflow-y-auto md:ml-64">
+        <header className="sticky top-0 z-30 flex items-center justify-between py-4 shadow md:py-6">
+          {/* Button to toggle sidebar on mobile */}
+          <button
+            className="px-4 text-black focus:outline-none"
+            onClick={toggleSidebar}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-6 h-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16m-7 6h7"
+              />
+            </svg>
+          </button>
+          <div className="relative flex flex-col items-center gap-x-2 "></div>
+        </header>
+
+        <main className="p-4 md:p-6">
           {ShowContent1 && (
             <div className="flex flex-col min-h-screen">
-              <div className="sticky top-0 z-40 w-full px-2 py-4 mb-2 xl:mb-10 bg-slate-800">
-                <h1 className="px-4 text-2xl font-bold border-l-4 border-yellow-400 text-slate-200">
+              <div className="sticky top-0 z-40 flex flex-row justify-between w-full px-2 py-4 mb-2 bg-black xl:mb-10">
+                <h1 className="px-4 text-xl font-medium border-l-4 border-yellow-400 text-slate-200">
                   {Gname}
                 </h1>
+                <button
+                  className="text-gray-300 md:hidden focus:outline-none"
+                  onClick={toggleSidebar}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16m-7 6h7"
+                    />
+                  </svg>
+                </button>
               </div>
               <div className="flex flex-col">
-                <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-x-1">
+                <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-x-1">
                   {dataFilterd?.slice(0, count).map((data, index) => {
                     return (
                       <div key={data.id + index}>
@@ -156,10 +240,10 @@ function Genres() {
                   })}
                 </div>
                 <div className="flex items-end justify-end px-10 py-4 text-base xl:text-lg gap-x-5">
-                  <h1 className="font-semibold  text-slate-200">Page {1}</h1>
+                  <h1 className="font-semibold text-slate-200">Page {1}</h1>
                   <h1
                     onClick={handleIncrese}
-                    className="font-semibold cursor-pointer  text-slate-200"
+                    className="font-semibold cursor-pointer text-slate-200"
                   >
                     More..
                   </h1>
@@ -169,13 +253,32 @@ function Genres() {
           )}
           {ShowContent2 && (
             <div className="flex flex-col min-h-screen">
-              <div className="sticky top-0 z-40 w-full px-2 py-4 mb-2 xl:mb-10 bg-slate-800">
+              <div className="sticky top-0 z-40 flex flex-row justify-between w-full px-2 py-4 mb-2 bg-black xl:mb-10 ">
                 <h1 className="px-4 text-2xl font-bold border-l-4 border-yellow-400 text-slate-200 ">
                   {Gnames}
                 </h1>
+                <button
+                  className="text-gray-300 md:hidden focus:outline-none"
+                  onClick={toggleSidebar}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16m-7 6h7"
+                    />
+                  </svg>
+                </button>
               </div>
               <div className="flex flex-col ">
-                <div className="grid grid-cols-3 p-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-x-1">
+                <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-x-1">
                   {TvdataFilter?.slice(0, counts).map((data, index) => {
                     return (
                       <div key={data?.id + index}>
@@ -204,7 +307,7 @@ function Genres() {
               </div>
             </div>
           )}
-        </div>
+        </main>
       </div>
     </div>
   );
