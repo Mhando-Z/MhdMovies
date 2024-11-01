86a0604d23c07a108842eb7ae3b17cfe8e26aad0
Mhando-Z <Mhandosz17@gmail.com>
2024-11-02 01:43:58 +0300

    Phase Three

--- src/UpGrade/TvSeriesScreens/AiringToday.jsx
+++ src/UpGrade/TvSeriesScreens/AiringToday.jsx
@@ -5,6 +5,7 @@ import TvSeriesContex from "../../Context/TvSeriesContext";
 import { Dots } from "react-activity";
 import { Rating } from "../Components/Collection";
 import { BsCalendarEvent } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 const AiringToday = () => {
   const [activeIndex, setActiveIndex] = useState(0);
@@ -38,7 +39,7 @@ const AiringToday = () => {
       >
         {ToDay?.map((day, index) => (
           <motion.div
-            key={index}
+            key={index + day.id}
             initial={{ opacity: 0, x: 50 }}
             animate={{ opacity: 1, x: 0 }}
             transition={{ delay: index * 0.2 }}
@@ -48,56 +49,60 @@ const AiringToday = () => {
                 : "min-w-[250px]"
             } transition-all duration-300`}
           >
-            {/* poster image overlay  */}
-            <div className="absolute top-0 bottom-0 left-0 right-0">
-              <img
-                src={`https://image.tmdb.org/t/p/original/${
-                  index === activeIndex ? day?.backdrop_path : day?.poster_path
-                }`}
-                alt={day?.name}
-                className="object-cover w-full h-full"
-              />
-              <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent via-50% to-transparent" />
-            </div>
+            <Link to={`/TvSeriesDetails/${day?.id}`}>
+              {/* poster image overlay  */}
+              <div className="absolute top-0 bottom-0 left-0 right-0">
+                <img
+                  src={`https://image.tmdb.org/t/p/original/${
+                    index === activeIndex
+                      ? day?.backdrop_path
+                      : day?.poster_path
+                  }`}
+                  alt={day?.name}
+                  className="object-cover w-full h-full"
+                />
+                <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent via-50% to-transparent" />
+              </div>
 
-            {/* Content Container */}
-            <div className="absolute bottom-0 left-0 right-0 z-10 px-5">
-              {/* Series Title */}
-              <motion.h2
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                whileInView={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.6 }}
-                className={`text-2xl font-bold text-white ${
-                  index === activeIndex ? "" : "hidden"
-                }`}
-              >
-                {day?.name}
-                <span className="flex items-center gap-2">
-                  <span className="font-medium">
-                    {day?.vote_average?.toFixed(1)}
+              {/* Content Container */}
+              <div className="absolute bottom-0 left-0 right-0 z-10 px-5">
+                {/* Series Title */}
+                <motion.h2
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  whileInView={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.6 }}
+                  className={`text-2xl font-bold text-white ${
+                    index === activeIndex ? "" : "hidden"
+                  }`}
+                >
+                  {day?.name}
+                  <span className="flex items-center gap-2">
+                    <span className="font-medium">
+                      {day?.vote_average?.toFixed(1)}
+                    </span>
+                    <Rating value={day?.vote_average} />
+                    <BsCalendarEvent className="w-4 h-4" />
+                    <span>{day?.first_air_date?.split("-")[0]}</span>
                   </span>
-                  <Rating value={day?.vote_average} />
-                  <BsCalendarEvent className="w-4 h-4" />
-                  <span>{day?.first_air_date?.split("-")[0]}</span>
-                </span>
-              </motion.h2>
+                </motion.h2>
 
-              {/* Series Overview */}
-              <motion.p
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                whileInView={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.6 }}
-                className={`${
-                  index === activeIndex
-                    ? "text-white/80 line-clamp-2 md:line-clamp-none"
-                    : "hidden"
-                }`}
-              >
-                {day?.overview}
-              </motion.p>
-            </div>
+                {/* Series Overview */}
+                <motion.p
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  whileInView={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.6 }}
+                  className={`${
+                    index === activeIndex
+                      ? "text-white/80 line-clamp-2 md:line-clamp-none"
+                      : "hidden"
+                  }`}
+                >
+                  {day?.overview}
+                </motion.p>
+              </div>
+            </Link>
           </motion.div>
         ))}
       </motion.div>
